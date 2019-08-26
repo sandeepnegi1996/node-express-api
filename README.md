85096a4ddceee181031749641e6e08119205603d
sandeepnegi1996 <9041128179sandy@gmail.com>
2019-08-26 13:24:06 +0530

    created README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# node js api using express
+
+### express is a framework by which it is easier to create an api
+
+```javascript
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.listen(3000, () => {
+  console.log("listening");
+});
+```
--- app.js
+++ app.js
@@ -5,6 +5,15 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+app.get("/api/getlatest", (req, res) => {
+  res.send([
+    { deviceId: 1, temperature: 23 },
+    { deviceId: 2 },
+    { deviceId: 3 }
+  ]);
+  //inside the send we are returning an array of objects
+});
+
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
